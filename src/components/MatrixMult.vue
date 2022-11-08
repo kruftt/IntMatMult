1540ac09f8b74d4d4be15a98371dab39fa0423bc
Marc Doucette <nnrbgb@gmail.com>
2022-11-08 11:49:23 -0800

    basic functionality
    implement svg diagrams, build for ghpages

--- src/components/MatrixMult.vue
+++ src/components/MatrixMult.vue
@@ -1,18 +1,81 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
+import {
+  computed,
+  ref,
+  reactive,
+  onMounted
+} from 'vue'
 
 const v_x = ref<SVGGElement|null>(null)
 
-const A = reactive([[1,0],[0,1]]) // by rows
-const x = reactive([1,1])
-const b = reactive([1,1])
+const A = reactive([[3,1],[-1,2]]) // by rows
+const x = reactive([2,2])
+const b = reactive([8,2])
 
-const hp1 = reactive([0,0]) // y values at x = -10, 10
-const hp2 = reactive([0,0]) // y values at x = -10, 10
+// const hp1 = reactive([0,0]) // y values at x = -10, 10
+// const hp2 = reactive([0,0]) // y values at x = -10, 10
+// const hp1 = computed(() => [b[0]/A[0][0], b[0]/A[0][1]])
+// const hp2 = computed(() => [b[1]/A[1][0], b[1]/A[1][1]])
 
-// am I even using vue here?
-// can I bind these vector values automatically?
-// 
+// remember that line is perpendicular to the vector!
+function getLineCoords(x: number, y: number, b: number) {
+  if (y == 0) {
+    if (x == 0) {
+      return [0, 0, 0, 0]
+    } else {
+      // vertical line intercept b/x
+      const i = b/x
+      return [i, 10, i, -10]
+    }
+  } else {
+    if (x == 0) {
+      // horizontal line
+      const i = b/y
+      return [-10, i, 10, i]
+    } else {
+      if (Math.abs(x) < Math.abs(y)) {
+        // todo optimize multiplications
+        const m = -x/y
+        const i = b/y // y intercept
+        const dy = 10*m
+        return [-10, i-dy, 10, i+dy]
+      } else {
+        const m = -y/x
+        const i = b/x
+        const dx = 10*m
+        return [i-dx ,-10, i+dx, 10]
+      }
+    }
+  }
+ 
+  // const m = -y_int/x_int
+  // console.log('GLC: ', x_int, y_int, m)
+  // const y1 = -m*(10+x_int) // x = -10
+  // const y2 = m*(10-x_int) // x = 10
+  // return [y1, y2]
+}
+
+const hp1 = computed(() => {
+  return getLineCoords(A[0][0], A[0][1], b[0])
+})
+
+const hp2 = computed(() => {
+  return getLineCoords(A[1][0], A[1][1], b[1])
+})
+
+const xcol1 = computed(() => [x[0]*A[0][0], x[0]*A[1][0]])
+const xcol2 = computed(() => [x[1]*A[0][1], x[1]*A[1][1]])
+
+// g(class="vec col2 xcol")
+//   line(:x1="xcol1[0]" :y1="xcol1[1]" :x2="b[0]" :y2="b[1]")
+//   use(href="#arrowhead" class="arrowhead" :x="b[0]-0.4" :y="b[1]-0.4" :transform="vTransform(A[0][1], A[1][1], b[0], b[1])")
+
+// const scol = computed(() => [xcol1[0]+xcol])
+
+
+const _deg = 180/Math.PI
+const vTransform = (x: number, y: number, cx=x, cy=y) => `rotate(${_deg * Math.atan2(y, x) - 90}, ${cx}, ${cy})`
+
 
 function compute_b() {
   b[0] = A[0][0]*x[0] + A[0][1]*x[1]
@@ -21,17 +84,15 @@ function compute_b() {
 
 function compute_x() {
   const inv_det = 1 / (A[0][0]*A[1][1] - A[1][0]*A[0][1])
-  x[0] = inv_det * (A[1][1] - A[0][1])
-  x[1] = inv_det * (A[0][0] - A[1][0])
+  x[0] = inv_det * (A[1][1]*b[0] - A[0][1]*b[1])
+  x[1] = inv_det * (A[0][0]*b[1] - A[1][0]*b[0])
 }
 
-// watch A, x -> compute b
-// watch b -> compute x
+// A, x -> compute b
+// b -> compute x
 // computed object with lines, dependent on A, b
-// watch A, x, b -> update arrowheads
+// A, x, b -> update arrowheads
 
-
-const _deg = -180/Math.PI
 function updateVector(e: SVGGElement, x1: number, y1: number, x2: number, y2: number) {
   const l = e.querySelector('line')
   l!.setAttribute('x1', `${x1}`)
@@ -54,13 +115,11 @@ function updateLine(e: SVGGElement, y: number, x: number) {
   const y1 = -m*(10+x) // x = -10
   const y2 = m*(10-x) // x = 10
   const l = e.querySelector('line')
-
 }
-
 
 onMounted(() => {
   // xAxis.value!.setAttribute("stroke", "#f0f")
-  updateVector(v_x.value!, 0, 0, -3, -1.5)
+  // updateVector(v_x.value!, 0, 0, -3, -1.5)
 })
 </script>
 
@@ -69,58 +128,111 @@ onMounted(() => {
 <template lang="pug">
 div(id="matrix_mult")
   div(id="graphs")
-    svg(class="graph" id="domain" width="30em" height="30em" viewBox="-10 -10 20 20")
-      g(stroke="#555" stroke-width="0.1")
-        line(x1="0" y1="-10" x2="0" y2="10")
-        line(x1="-10" y1="0" x2="10" y2="0")
-      symbol(id="arrowhead" width="0.6" height="0.6" viewBox="-1 0 2 2")
-        polygon(points="-1,0 1,0 0,1.732")
-      
-      g(id="hp1" class="hp col1")
-        line(x1="-6" y1="-10" x2="10" y2="6")
-      
-      g(id="hp2" class="hp col2")
-        line(x1="10" y1="-6" x2="-6" y2="10")
-      
-      g(id="col1" class="vec col1")
-        line(x1="0" y1="0" x2="5" y2="-5")
-        use(href="#arrowhead" class="arrowhead" x="4.7" y="-5.3" transform="rotate(-135, 5, -5)")
-      
-      g(id="col2" class="vec col2")
-        line(x1="0" y1="0" x2="3" y2="3")
-        use(href="#arrowhead" class="arrowhead" x="2.7" y="2.7" transform="rotate(-45, 3, 3)")
-      
-      g(ref="v_x" class="vec x")
-        line(x1="0" y1="0" x2="4" y2="0")
-        use(href="#arrowhead" class="arrowhead" x="3.7" y="-0.3" transform="rotate(-90, 4, 0)")
-
-
-    svg(class="graph" id="codomain" width="30em" height="30em" viewBox="-10, -10, 20, 20")
-      g(stroke="#555" stroke-width="0.1")
+    div
+      h4 Domain
+      svg(class="graph" id="domain" width="30em" height="30em" viewBox="-10 -10 20 20" transform="scale(1,-1)")
+        g(stroke="#555" stroke-width="0.1")
           line(x1="0" y1="-10" x2="0" y2="10")
           line(x1="-10" y1="0" x2="10" y2="0")
+        symbol(id="arrowhead" width="0.8" height="0.8" viewBox="-1 -1 2 2")
+          polygon(points="-0.3,-1 0.3,-1 0,0")
+        
+        g(id="hp1" class="hp row1")
+          line(:x1="hp1[0]" :y1="hp1[1]" :x2="hp1[2]" :y2="hp1[3]")
+          //- line(x1="-6" y1="-10" x2="10" y2="6")
+        
+        g(id="hp2" class="hp row2")
+          line(:x1="hp2[0]" :y1="hp2[1]" :x2="hp2[2]" :y2="hp2[3]")
+          //- line(x1="10" y1="-6" x2="-6" y2="10")
+        
+        g(id="row1" class="vec row1")
+          line(x1="0" y1="0" :x2="A[0][0]" :y2="A[0][1]")
+          use(href="#arrowhead" class="arrowhead" :x="A[0][0]-0.4" :y="A[0][1]-0.4" :transform="vTransform(A[0][0], A[0][1])")
+        
+        g(id="row2" class="vec row2")
+          line(x1="0" y1="0" :x2="A[1][0]" :y2="A[1][1]")
+          use(href="#arrowhead" class="arrowhead" :x="A[1][0]-0.4" :y="A[1][1]-0.4" :transform="vTransform(A[1][0], A[1][1])")
+        
+        g(ref="v_x" class="vec x")
+          line(x1="0" y1="0" :x2="x[0]" :y2="x[1]")
+          use(href="#arrowhead" class="arrowhead" :x="x[0]-0.4" :y="x[1]-0.4" :transform="vTransform(x[0], x[1])")
 
-      g(id="row1" class="vec row1")
-        line(x1="0" y1="0" x2="1" y2="-1.732")
-        use(href="#arrowhead" class="arrowhead" x="0.7" y="-2.32" transform="rotate(-150, 1, -1.732)")
-      
-      g(id="row2" class="vec row2")
-        line(x1="1" y1="-1.732" x2="6" y2="0")
-        use(href="#arrowhead" class="arrowhead" x="5.7" y="-0.3" transform="rotate(-70, 6, 0)")
+    div
+      h4 Codomain
+      svg(class="graph" id="codomain" width="30em" height="30em" viewBox="-10, -10, 20, 20" transform="scale(1,-1)")
+        g(stroke="#555" stroke-width="0.1")
+            line(x1="0" y1="-10" x2="0" y2="10")
+            line(x1="-10" y1="0" x2="10" y2="0")
 
-      g(id="b" class="vec b")
-        line(x1="0" y1="0" x2="6" y2="0")
-        use(href="#arrowhead" class="arrowhead" x="5.7" y="-0.3" transform="rotate(-90, 6, 0)")
-      
+        g(class="vec col1 xcol")
+          line(x1="0" y1="0" :x2="xcol1[0]" :y2="xcol1[1]")
+          use(href="#arrowhead" class="arrowhead" :x="xcol1[0]-0.4" :y="xcol1[1]-0.4" :transform="vTransform(xcol1[0], xcol1[1])")
+        
+        g(class="vec col2 xcol")
+          line(:x1="xcol1[0]" :y1="xcol1[1]" :x2="xcol1[0]+xcol2[0]" :y2="xcol1[1]+xcol2[1]")
+          use(href="#arrowhead" class="arrowhead" :x="b[0]-0.4" :y="b[1]-0.4" :transform="vTransform(xcol2[0], xcol2[1], b[0], b[1])")
 
-  div(id="equation") equation
+        g(id="col1" class="vec col1")
+          line(x1="0" y1="0" :x2="A[0][0]" :y2="A[1][0]")
+          use(href="#arrowhead" class="arrowhead" :x="A[0][0]-0.4" :y="A[1][0]-0.4" :transform="vTransform(A[0][0], A[1][0])")
+        
+        g(id="col2" class="vec col2")
+          line(x1="0" y1="0" :x2="A[0][1]" :y2="A[1][1]")
+          use(href="#arrowhead" class="arrowhead" :x="A[0][1]-0.4" :y="A[1][1]-0.4" :transform="vTransform(A[0][1], A[1][1])")
+
+        g(id="b" class="vec b")
+          line(x1="0" y1="0" :x2="b[0]" :y2="b[1]")
+          use(href="#arrowhead" class="arrowhead" :x="b[0]-0.4" :y="b[1]-0.4" :transform="vTransform(b[0], b[1])")
+        
+
+  div(id="equation")
+    div [
+    table
+      tr
+        td
+          input(type="number" class="num_input" v-model="A[0][0]" @input="compute_b" step="0.1")
+        td
+          input(type="number" class="num_input" v-model="A[0][1]" @input="compute_b" step="0.1")
+      tr
+        td
+          input(type="number" class="num_input" v-model="A[1][0]" @input="compute_b" step="0.1")
+        td
+          input(type="number" class="num_input" v-model="A[1][1]" @input="compute_b" step="0.1")
+    div ][
+    table
+      tr
+        td
+          input(type="number" class="num_input" v-model="x[0]" @input="compute_b" step="0.1")
+      tr
+        td
+          input(type="number" class="num_input" v-model="x[1]" @input="compute_b" step="0.1")
+    div ] = [
+    table
+      tr
+        td
+          input(type="number" class="num_input" v-model="b[0]" @input="compute_x" step="0.1")
+      tr
+        td
+          input(type="number" class="num_input" v-model="b[1]" @input="compute_x" step="0.1")
+    div ]
 </template>
 
 
 <style scoped lang="stylus">
+// #matrix_mult
+//   width: 100%
+
+h4
+  margin 0.5em
+
+#graphs
+  display flex
+  flex-wrap wrap
+  justify-content center
+  gap 3em
+
 .graph
   border 2px solid black
-  margin 2em
 
 .arrowhead
   &:hover
@@ -128,16 +240,16 @@ div(id="matrix_mult")
 
 .hp
   stroke-width 0.08
-  opacity 0.5
+  opacity 0.25
 
 .vec
   stroke-width 0.1
 
-.col1
+.row1
   fill #FF0
   stroke #FF0
 
-.col2
+.row2
   fill #F80
   stroke #F80
 
@@ -145,15 +257,43 @@ div(id="matrix_mult")
   fill #F00
   stroke #F00
 
-.row1
+.col1
   fill #0F4
   stroke #0F4
 
-.row2
+.col2
   fill #0DD
   stroke #0DD
 
+.xcol
+  opacity 0.25
+
 .b
-  fill #80F
-  stroke #80F
+  fill #A0F
+  stroke #A0F
+
+#equation
+  display flex
+  justify-content center
+  align-items center
+  gap 0.05em
+
+  font-size 7em
+  color #666
+  // text-align center
+
+.num_input
+  border none
+  background-color #0000
+  text-align center
+  width 2.9em
+  padding 0.5em 0
+  white-space nowrap
+  overflow hidden
+  text-overflow clip
+  &:focus
+    outline none
+
+table
+  margin 0 0 0 0.1em
 </style>
